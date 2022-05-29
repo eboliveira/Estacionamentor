d65ccef33b399fb3d99e20243a5aab03330d0783
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2022-05-29 12:48:33 -0300

    feat(components): added entrance tab

--- app/src/main/java/com/example/estacionamentor/ui/components/Entrance.kt
+++ app/src/main/java/com/example/estacionamentor/ui/components/Entrance.kt
@@ -1,17 +1,41 @@
 package com.example.estacionamentor.ui.components
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.estacionamentor.R
 import com.example.estacionamentor.ui.theme.EstacionamentorTheme
+import com.example.estacionamentor.ui.theme.Success
 
 @Composable
 fun Entrance() {
     Scaffold {
-        Text("Hello World!")
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            val plate = ""
+            Spacer(Modifier.height(36.dp))
+            PlateInputField(plate)
+            Spacer(Modifier.height(16.dp))
+            Button(
+                backgroundColor = Success,
+                text = stringResource(id = R.string.confirm_entrance)
+            ) {
+                confirmEntrance()
+            }
+        }
     }
+}
+
+private fun confirmEntrance() {
+
 }
 
 @Preview
--- app/src/main/java/com/example/estacionamentor/ui/components/Exit.kt
+++ app/src/main/java/com/example/estacionamentor/ui/components/Exit.kt
@@ -31,7 +31,7 @@ fun Exit() {
     }
 }
 
-fun exit() {
+private fun exit() {
 
 }
 
