4590b7f1fb3382b8e6aac395f69fb471a968c659
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2022-05-29 12:40:13 -0300

    feat(exit): added exit tab

--- app/src/main/java/com/example/estacionamentor/ui/components/Tabs.kt
+++ app/src/main/java/com/example/estacionamentor/ui/components/Tabs.kt
@@ -2,6 +2,7 @@ package com.example.estacionamentor.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -9,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.estacionamentor.R
 import com.example.estacionamentor.ui.theme.EstacionamentorTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -21,7 +23,7 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun Tabs(pagerState: PagerState) {
-    Scaffold {
+    Scaffold(modifier = Modifier.padding(horizontal = 8.dp)) {
         Column {
             val scope = rememberCoroutineScope()
             TabRow(
